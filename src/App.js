330c4e80f1a0add25d13379d27dd35a7a3920b65
stephanie <stephieplusthree@gmail.com>
2020-10-29 14:06:57 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+
+      <div className="app__header">
+        <img 
+          className="app__headerImage"
+          src="https://toogreen.ca/instagreen/img/instagreen.svg"
+          height="40px;"
+          alt=""
+        />
+      </div>
+
+      {/* posts */}
+      {/* posts */}s
     </div>
   );
 }
