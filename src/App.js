38ceff80c0698ebe66d7534ced08ec3291672e70
stephanie <stephieplusthree@gmail.com>
2020-10-29 20:47:06 -0700

    posts component created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Post from './Post';
 
 function App() {
   return (
@@ -15,8 +16,10 @@ function App() {
         />
       </div>
 
-      {/* posts */}
-      {/* posts */}s
+      <Post username="stephieplusthree" caption="WOW, it works" imageUrl="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg" />
+      <Post username="Ryan" caption="Awesome" imageUrl="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$" />
+      <Post username="Malia" caption="Fantastic" imageUrl="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg" />
+
     </div>
   );
 }
